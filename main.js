7891f5ebb08c8f51bd4bb270d6a7cb99345b411c
Danish <khandanish36408@gmail.com>
2023-09-24 15:40:10 +0530

    start organize

---
+++ main.js
@@ -0,0 +1,15 @@
+
+let inputArr = process.argv.slice(2);
+let command = inputArr[0];
+switch (command) {
+    case "tree":
+        break;
+    case "organize":
+        break;
+    case "help":
+        break;
+    default:
+        console.log("command not recognize");
+        break;
+        
+}
